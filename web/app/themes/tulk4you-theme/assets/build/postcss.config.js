28214ab5db57d4fda916b51dd9332ff42e3fbb0e
Kirill Vitchenko <vitchenko.kirill@gmail.com>
2021-05-15 22:36:48 +0300

    top header

--- web/app/themes/tulk4you-theme/assets/build/postcss.config.js
+++ web/app/themes/tulk4you-theme/assets/build/postcss.config.js
@@ -4,9 +4,11 @@ const postcssNormalize = require('postcss-normalize')
 const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
 const gradientTransparencyFix = require('postcss-gradient-transparency-fix')
+const tailwindcss = require('tailwindcss')
 
 module.exports = {
 	plugins: [
+		tailwindcss(),
 		autoprefixer(),
 		postcssNormalize(),
 		cssnano(
---
+++ web/app/themes/tulk4you-theme/tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  theme: {
+    container : {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '0',
+        lg: '0',
+        xl: '0',
+        '2xl': '0',
+      },
+    },
+    colors: {
+      dark: '#131423',
+      red: '#c92456',
+      light: '#f4f4f4',
+    },
+    fontFamily: {
+      sans: ['Noto Serif', 'sans-serif'],
+      serif: ['Noto Serif', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '1': '10px',
+        '2': '20px',
+        '3': '30px',
+        '4': '40px',
+        '5': '50px',
+        '6': '60px',
+        '7': '70px',
+        '8': '80px',
+      },
+    }
+  }
+}

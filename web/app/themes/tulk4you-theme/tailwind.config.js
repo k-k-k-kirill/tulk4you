588b1312f9a5569bcd2de3f2d3a74a3c02100a23
Kirill Vitchenko <vitchenko.kirill@gmail.com>
2022-06-07 22:02:14 +0300

    gtm codes

--- web/app/themes/tulk4you-theme/tailwind.config.js
+++ web/app/themes/tulk4you-theme/tailwind.config.js
@@ -1,38 +1,38 @@
 module.exports = {
-  theme: {
-    container : {
-      center: true,
-      padding: {
-        DEFAULT: '1rem',
-        sm: '1rem',
-        lg: '1rem',
-        xl: '0',
-        '2xl': '0',
-      },
-    },
-    colors: {
-      dark: '#131423',
-      red: '#c92456',
-      light: '#f4f4f4',
-    },
-    fontFamily: {
-      sans: ['Noto Serif', 'sans-serif'],
-      serif: ['Noto Serif', 'serif'],
-    },
-    extend: {
-      spacing: {
-        '1': '10px',
-        '2': '20px',
-        '3': '30px',
-        '4': '40px',
-        '5': '50px',
-        '6': '60px',
-        '7': '70px',
-        '8': '80px',
-        '9': '90px',
-        '10': '100px',
-        '12': '120px',
-      },
-    }
-  }
-}
+	theme: {
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: "1rem",
+				sm: "1rem",
+				lg: "1rem",
+				xl: "0",
+				"2xl": "0",
+			},
+		},
+		colors: {
+			dark: "#131423",
+			red: "#c92456",
+			light: "#f4f4f4",
+		},
+		fontFamily: {
+			sans: ["Noto Serif", "sans-serif"],
+			serif: ["Noto Serif", "serif"],
+		},
+		extend: {
+			spacing: {
+				1: "10px",
+				2: "20px",
+				3: "30px",
+				4: "40px",
+				5: "50px",
+				6: "60px",
+				7: "70px",
+				8: "80px",
+				9: "90px",
+				10: "100px",
+				12: "120px",
+			},
+		},
+	},
+};
